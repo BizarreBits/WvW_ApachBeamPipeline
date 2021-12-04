f80b974fe6a9a166cd977991dafacd47d33e6e1b
BizarreBits <63097150+BizarreBits@users.noreply.github.com>
2021-12-04 16:38:56 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import com.google.common.collect.ImmutableMap;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.values.KV;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class Main
+{
+
+}
